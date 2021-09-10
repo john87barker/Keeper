34613ff04294fb1f60f7f6855a02f6bc5faaeaaa
john87barker <john87barker@gmail.com>
2021-09-10 11:11:34 -0600

    Account set up

---
+++ Keeper.client/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : 'http://localhost:8080'
+export const domain = 'john87barker.us.auth0.com'
+export const audience = 'https://keeper.com'
+export const clientId = 'QQusmGo8lhCmNPMZQN1UilQemeqBJHP1'
